44b9a9082fb7788a5a8a554605416fbd8b46bf8b
yezigege <18839136833@163.com>
2019-09-23 11:06:44 +0800

    初步可以返回消息

--- wxauthorize.py
+++ wxauthorize.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-
+import time
 
 import hashlib
 import tornado.web
-# from core.logger_helper import logger
+# from core.logger_helper import
+import xml.etree.ElementTree as ET
 import logging
 
 
@@ -43,3 +44,49 @@ class WxSignatureHandler(tornado.web.RequestHandler):
         sha1 = hashlib.sha1(s.encode('utf-8')).hexdigest()
         logging.debug('sha1=' + sha1 + '&signature=' + signature)
         return sha1 == signature
+
+    def post(self):
+        body = self.request.body
+        logging.error('微信消息回复中心】收到用户消息 \n' + str(body.decode()))
+        data = ET.fromstring(body)
+        ToUserName = data.find('ToUserName').text
+        FromUserName = data.find('FromUserName').text
+        MsgType = data.find('MsgType').text
+        if MsgType == 'text' or MsgType == 'voice':
+            '''文本消息 or 语音消息'''
+            try:
+                MsgId = data.find("MsgId").text
+                if MsgType == 'text':
+                    Content = data.find('Content').text  # 文本消息内容
+                elif MsgType == 'voice':
+                    Content = data.find('Recognition').text  # 语音识别结果，UTF8编码
+                if Content == u'你好':
+                    reply_content = '您好,请问有什么可以帮助您的吗?'
+                else:
+                    # 查找不到关键字,默认回复
+                    reply_content = "小叶子正在慢悠悠的开发中~"
+                if reply_content:
+                    CreateTime = int(time.time())
+                    out = self.reply_text(FromUserName, ToUserName, CreateTime, reply_content)
+                    self.write(out)
+            except:
+                pass
+
+        elif MsgType == 'event':
+            '''接收事件推送'''
+            try:
+                Event = data.find('Event').text
+                if Event == 'subscribe':
+                    # 关注事件
+                    CreateTime = int(time.time())
+                    reply_content = self.sys_order_reply
+                    out = self.reply_text(FromUserName, ToUserName, CreateTime, reply_content)
+                    self.write(out)
+            except:
+                pass
+
+    def reply_text(self, FromUserName, ToUserName, CreateTime, Content):
+        """回复文本消息模板"""
+        textTpl = """<xml> <ToUserName><![CDATA[%s]]></ToUserName> <FromUserName><![CDATA[%s]]></FromUserName> <CreateTime>%s</CreateTime> <MsgType><![CDATA[%s]]></MsgType> <Content><![CDATA[%s]]></Content></xml>"""
+        out = textTpl % (FromUserName, ToUserName, CreateTime, 'text', Content)
+        return out
